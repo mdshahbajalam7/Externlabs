67d6c6855cf7150d1f738b12ec17a894aa862712
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2022-10-03 19:47:26 +0530

    done

--- assignment-f/src/components/Slider.jsx
+++ assignment-f/src/components/Slider.jsx
@@ -3,15 +3,14 @@ import { useEffect } from "react";
 import { useState } from "react";
 import styles from "../components/Slider.module.css";
 let slid = [
-  "https://static.javatpoint.com/top10-technologies/images/top-10-hollywood-action-movies3.png",
-  "https://www.pensivly.com/wp-content/uploads/2020/04/THE-WITCHER-2019-Hollywood-Movies-in-Hindi-Dubbed-Full-Action.jpg",
-  "https://www.pensivly.com/wp-content/uploads/2020/04/THE-WITCHER-2019-Hollywood-Movies-in-Hindi-Dubbed-Full-Action.jpg",
-  "https://static.javatpoint.com/top10-technologies/images/top-10-hollywood-action-movies4.png",
-  "https://static.javatpoint.com/top10-technologies/images/top-10-hollywood-action-movies4.png",
-  "https://static.javatpoint.com/top10-technologies/images/top-10-hollywood-action-movies4.png",
-  "https://static.javatpoint.com/top10-technologies/images/top-10-hollywood-action-movies9.png",
-  "https://phando021.s.llnwi.net/public/upload/news/story_mul_image_1611817900_2.jpg",
-//   "https://static.javatpoint.com/top10-technologies/images/top-10-hollywood-action-movies8.png",
+    "https://images.unsplash.com/photo-1664730302624-3cba2a181006?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1664575600850-c4b712e6e2bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1664574653006-4d7eb5f1a418?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1664776375021-136d055af349?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1664732479760-b65e7166bd5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1664764118950-ab69f4c40c82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1664739573730-11011f6690e9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1657299142317-00e647447f80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
 ];
 function Slider() {
   const [image, setimage] = useState(0);
